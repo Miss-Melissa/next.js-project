d3679742852d18861e4d341ce34585a24a2b3697
Melissa Zarinnegar <melissahh@live.se>
2023-02-22 12:16:15 +0100

    added events list data from dummmy data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
+import { getFeaturedEvents } from "dummy-data";
+import EventList from "@/components/events/event-list";
+
+
 function Home() {
+    const featuredEventes = getFeaturedEvents();
+
     return (
         <div>
             <h1>The Home Page</h1>
+            <EventList items={featuredEventes} />
         </div>
     )
 }
