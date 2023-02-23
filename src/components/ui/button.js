ada54a5095b182adf2a57476160eac52aa47a4ed
Melissa Zarinnegar <melissahh@live.se>
2023-02-23 11:01:31 +0100

    done

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -1,16 +1,21 @@
-import Link from "next/link";
-import styles from "./button.module.css";
+import Link from 'next/link';
+
+import styles from './button.module.css';
 
 function Button(props) {
   if (props.link) {
     return (
-      <Link href={props.link}>
-        <span className={styles.btn}>{props.children}</span>
+      <Link href={props.link} className={styles.btn}>
+        {props.children}
       </Link>
     );
   }
 
-  return <button onClick={props.onClick}>{props.children}</button>;
+  return (
+    <button className={styles.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
